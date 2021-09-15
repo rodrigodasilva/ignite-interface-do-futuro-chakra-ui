c2ffea4703387bd14ad7e939c8e148ce85db680c
rodrigodasilva <ribeiro.rodrigodasilva>
2021-09-15 19:42:32 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+<img alt="Ignite" src="https://i.imgur.com/eCVyxxy.png">
+<h2 align="center">
+  Ignite - Trilha ReactJS
+</h2>
+<p align="center">
+  Aula 04: Construindo aplicação do futuro
+</p>
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Instale as dependências com yarn
+- Inicie o servidor com yarn dev
+- Agora você pode acessar localhost:3000 do seu navegador.
---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,26 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Rodrigo Ribeiro</Text>
+          <Text color="gray.300" fontSize="small">
+            ribeiro.rodrigodasilva@gmail.com
+          </Text>
+        </Box>
+      )}
+
+      <Avatar
+        size="md"
+        name="Rodrigo Ribeiro"
+        src="https://github.com/rodrigodasilva.png"
+      />
+    </Flex>
+  );
+}
